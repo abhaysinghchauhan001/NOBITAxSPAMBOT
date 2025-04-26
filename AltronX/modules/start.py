6407bbb2e7b53679238ae5477e5b16fac72a3d78
abhaysinghchauhan001 <chauhanabhay206243@gmail.com>
2025-04-26 14:23:10 +0530

    Add files via upload

---
+++ AltronX/modules/start.py
@@ -0,0 +1,44 @@
+from telethon import events, Button
+from config import MK1, MK2, MK3, MK4, MK5, MK6, MK7, MK8, MK9, MK10
+from AltronX.modules.help import *
+import telethon
+
+PythonButton = [
+        [
+        Button.inline("⚡️𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦⚡️", data="help_back")
+        ],
+        [
+        Button.url("⚡️𝗨𝗣𝗗𝗔𝗧𝗘⚡️", "https://t.me/NOBITA_MUSIC_SUPPORT"),
+        Button.url("⚡️𝗦𝗨𝗣𝗣𝗢𝗥𝗧⚡️", "https://t.me/OG_DEFAULTERS_001")
+        ],
+        [
+        Button.url("⚡️𝗥𝗘𝗣𝗢⚡️", "𝗡𝗢𝗕𝗜𝗧𝗔 𝗣𝗔𝗣𝗔 𝗕𝗢𝗟 𝗥𝗘𝗣𝗢 𝗟𝗘𝗚𝗔 𝗟𝗔𝗨𝗗𝗘")
+        ]
+        ]
+
+
+@MK1.on(events.NewMessage(pattern="/start"))
+@MK2.on(events.NewMessage(pattern="/start"))
+@MK3.on(events.NewMessage(pattern="/start"))
+@MK4.on(events.NewMessage(pattern="/start"))
+@MK5.on(events.NewMessage(pattern="/start"))
+@MK6.on(events.NewMessage(pattern="/start"))
+@MK7.on(events.NewMessage(pattern="/start"))
+@MK7.on(events.NewMessage(pattern="/start"))
+@MK8.on(events.NewMessage(pattern="/start"))
+@MK9.on(events.NewMessage(pattern="/start"))
+@MK10.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+        AltBot = await event.client.get_me()
+        BotName = AltBot.first_name
+        BotId = AltBot.id
+        TEXT = f"**𝗛𝗘𝗬 [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n𝗜 𝗔𝗠  [{BotName}](tg://user?id={BotId})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+        TEXT += f"» **✦ 𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗗 𝗕𝗬 :~ [𝚴 𝐎 𝐁 𝚰 𝐓 𝚲](https://t.me/ll_YOURR_NOBITA_ll)**\n\n"
+        TEXT += f"» **𝗡𝗢𝗕𝗜𝗧𝗔 𝗦𝗣𝗔𝗠 𝗩𝗘𝗥𝗦𝗜𝗢𝗡 :** `3.2`\n"
+        TEXT += f"» **𝗧𝗘𝗟𝗘𝗧𝗛𝗢𝗡 𝗩𝗘𝗥𝗦𝗜𝗢𝗡:** `{telethon.__version__}`\n━━━━━━━━━━━━━━━━━"
+        await event.client.send_file(
+                event.chat_id,
+                "https://files.catbox.moe/7qyorb.jpg",
+                caption=TEXT, 
+                buttons=PythonButton)
